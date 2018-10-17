a1c4d6f3d78e213c6e2c5f9d04712057f897e06a
noel5550 <noel.nyunting@gmail.com>
2018-10-17 11:18:17 +0200

    Commit de Noel

---
+++ app/src/main/java/com/example/noel/webview/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.noel.webview;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.univ-nantes.fr");
+
+        //SETTINGS WEBVIEW
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setAllowContentAccess(true);
+
+    }
+
+    @Override
+    /*
+    *   Sur la touche back, on ne sort pas de l'appli tant qu'on peut revenir dans la page d'avant
+    */
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }else{
+            super.onBackPressed();
+        }
+    }
+}
